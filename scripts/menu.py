39fc6d58c7b43eb9e0aa431598c026808902a69e
igor alves <igoralvespalmeirag4@gmail.com>
2025-04-16 19:46:51 -0300

    refactor(menu): remover carregamento de transações de exemplo"

--- scripts/menu.py
+++ scripts/menu.py
@@ -16,8 +16,6 @@ class Menu:
         print("=================================")
     
     def executar(self):
-        self.carregar_dados_exemplo()
-        
         while True:
             self.exibir_menu()
             escolha = input("Escolha uma opção: ")
@@ -65,24 +63,3 @@ class Menu:
     def listar_categorias(self):
         resultado = self.carteira.listar_categorias()
         print(resultado)
-    
-    def carregar_dados_exemplo(self):
-        resposta = input("Deseja carregar transações de exemplo? (s/n): ").lower()
-        if resposta == 's':
-            transacoes = [
-                Transacao("Compra de café", -10.50, "Alimentação", "15/10/2023"),
-                Transacao("Salário", 2500.00, "Renda", "01/10/2023"),
-                Transacao("Compra de livro", -50.00, "Educação", "20/10/2023"),
-                Transacao("Aluguel", -1200.00, "Moradia", "05/10/2023"),
-                Transacao("Supermercado", -300.00, "Alimentação", "10/10/2023"),
-                Transacao("Venda de bicicleta", 500.00, "Renda Extra", "12/10/2023"),
-                Transacao("Cinema", -30.00, "Lazer", "18/10/2023"),
-                Transacao("Academia", -100.00, "Saúde", "08/10/2023"),
-                Transacao("Freelance", 800.00, "Renda Extra", "22/10/2023"),
-                Transacao("Doação", -50.00, "Caridade", "25/10/2023")
-            ]
-            
-            for transacao in transacoes:
-                self.carteira.adicionar(transacao)
-            
-            print("Transações de exemplo carregadas com sucesso!")
