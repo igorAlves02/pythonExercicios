f7fbb95222365e5eb37edfb836639270a02749bf
igor alves <igoralvespalmeirag4@gmail.com>
2025-04-25 16:00:05 -0300

    feat(rh): feito a classe rh

--- sistema_empresa/sistema_rh.py
+++ sistema_empresa/sistema_rh.py
@@ -1,48 +1,30 @@
-from funcionarios import Funcionario, FuncionarioComum, Gerente, logar_acao
+from funcionarios import Funcionario, FuncionarioComum, Gerente
 
-class SistemaRH():
+def logar_acao(func):
+    def wrapper(*args, **kwargs):
+        funcionario = args[1]
+        bonus = func(*args, **kwargs)
+        
+        cargo = "funcionário"
+        if isinstance(funcionario, Gerente):
+            cargo = "gerente"
+        
+        print(f"O bonus do {cargo} {funcionario.get_nome()} é de {bonus:.2f}")
+        return bonus
+    return wrapper
+
+
+class SistemaRH:
     def __init__(self):
         self.__lista_de_funcionarios = []
     
     def set_funcionario(self, funcionario: Funcionario):
         self.__lista_de_funcionarios.append(funcionario)
-        
+    
     @logar_acao
-    def __mostrar_bonus(self, funcionario: Funcionario) -> float:
+    def mostrar_bonus(self, funcionario: Funcionario) -> float:
         return funcionario.calcular_bonus()
-
+    
     def listar_bonuses(self):
         for funcionario in self.__lista_de_funcionarios:
             self.mostrar_bonus(funcionario)
-    
-if __name__ == "__main__":
-    rh = SistemaRH()
-    
-    funcionarios_comuns = [
-        FuncionarioComum("Alice",   3000.00),
-        FuncionarioComum("Carlos",  3500.00),
-        FuncionarioComum("Bruna",   4000.00),
-        FuncionarioComum("Daniel",  4500.00),
-        FuncionarioComum("Elisa",   5000.00),
-        FuncionarioComum("Fernando",5500.00),
-        FuncionarioComum("Gabriela",6000.00),
-        FuncionarioComum("Henrique",6500.00),
-        FuncionarioComum("Irene",   7000.00)
-    ]
-
-    gerentes = [
-        Gerente("João", 10000.00,   250.00),
-        Gerente("Karla",12500.00,   500.00),
-        Gerente("Lucas",15000.00,   750.00)
-    ]
-    
-    for index in range(9):
-       
-        if index % 3 == 0:
-            rh.set_funcionario(gerentes[index // 3])
-            rh.set_funcionario(funcionarios_comuns[index])
-        else:
-            rh.set_funcionario(funcionarios_comuns[index])
-            
-    rh.listar_bonuses()
-    
